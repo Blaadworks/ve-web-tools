02445cc56de4bdba1061031e23260d648dbdea51
Blaadick <lablaadick@gmail.com>
2026-06-21 03:27:24 +0300

    Update item description

--- src/features/rarity-preview/components/ItemCard.tsx
+++ src/features/rarity-preview/components/ItemCard.tsx
@@ -24,8 +24,8 @@ export function ItemCard({ colorTop, colorBottom }: Props) {
                     }}
                 />
             </div>
-            <b className="text-center">Железный меч</b>
-            <div>Самый обычный, ничем ни примечательный меч из стали.</div>
+            <b className="text-center">Steel Longsword</b>
+            <div>A perfectly ordinary, unremarkable steel sword</div>
         </div>
     )
 }
