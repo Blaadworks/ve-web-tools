ee0e168ac4400ec21cc020adb5a5d2e831cf7428
Sapryx <sapryxv@gmail.com>
2026-06-22 23:16:01 +0300

    updated item card

--- src/features/rarity-preview/components/ItemCard.tsx
+++ src/features/rarity-preview/components/ItemCard.tsx
@@ -8,43 +8,61 @@ type Props = {
 };
 
 export function ItemCard({ colorTop, colorBottom }: Props) {
-    const backgroundGradient = `linear-gradient(180deg, 
-        rgb(${colorTop.r} ${colorTop.g} ${colorTop.b}) 0%,
-        rgb(${colorBottom.r} ${colorBottom.g} ${colorBottom.b}) 100%
+    const backgroundGradient = `linear-gradient(
+        rgb(${colorTop.r}, ${colorTop.g}, ${colorTop.b}),
+        rgb(${colorBottom.r}, ${colorBottom.g}, ${colorBottom.b})
     )`;
 
     return (
-        <div
-            className={style({
-                width: 300,
-                height: 200,
+        <div className={style({
+            width: 300,
+            height: 200,
+            display: "flex",
+            flexDirection: "column",
+            borderRadius: "default",
+        })} style={{
+            backgroundImage: backgroundGradient,
+        }}>
+            <div className={style({
                 display: "flex",
-                flexDirection: "column",
-                gap: 12,
-                padding: 16,
+                justifyContent: "center",
+                alignItems: "center",
+                height: 36,
+            })}>
+                <Text styles={style({
+                    font: "title",
+                })}>
+                    Железный меч
+                </Text>
+            </div>
+
+            <div className={style({
+                flex: 1,
+                height: "100%",
                 borderRadius: "default",
-            })}
-            style={{ backgroundImage: backgroundGradient }}
-        >
-            <img
-                src="https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8e/Iron_Sword_JE2_BE2.png"
-                alt="Железный меч"
-                className={style({
-                    width: 64,
-                    alignSelf: "center",
-                })}
-            />
-            <Text styles={style({
-                font: "title",
-                textAlign: "center",
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7))`,
+                margin: 4,
             })}>
-                Железный меч
-            </Text>
-            <Text styles={style({
-                font: "body",
-            })}>
-                Обычный, ничем не примечательный меч из стали.
-            </Text>
+                <div className={style({
+                    padding: 16,
+                    flexDirection: "column",
+                })}>
+                    <img
+                        src="https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8e/Iron_Sword_JE2_BE2.png"
+                        alt="Железный меч"
+                        className={style({
+                            width: 48,
+                            alignSelf: "center",
+                        })}
+                    />
+
+                    <Text styles={style({
+                        font: "body",
+                    })}>
+                        Обычный, ничем ни примечательный меч из стали.
+                    </Text>
+                </div>
+            </div>
         </div>
     );
 }
